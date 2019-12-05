616e2ca8a2cfdf173ba8f5fd970fba92de8d8743
Emmanuel Sunday Akomaning <sundux7@gmail.com>
2019-12-05 17:53:46 +0000

    added build packs

--- resources/views/password/activation_success.blade.php
+++ resources/views/password/activation_success.blade.php
@@ -10,7 +10,7 @@
       <section style="padding: 17rem 0 10rem;">
         <div class="container">
           <div class="row text-center">
-            <div class="col-md-12 col-lg-12">
+            <div class="col-sm-12 col-md-12 col-lg-12">
               <h1 class="display-3">Account Activated 👍</h1>
 
               <p class="h4">You can now  <a href="/login">Login</a>. </p>
